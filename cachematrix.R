1e853e160af76acab69985a6063b79a27d7c7fd8
anuragsirish <anurag.sirish@gmail.com>
2016-08-09 17:06:10 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+    makeMatrix <- function(origmatrix = matrix()) {
+            invmatrix <- NULL
+            set <- function(y) {
+                    origmatrix <<- y
+                    invmatrix <<- NULL
+            }
+            
+            
+            get <- function() origmatrix
+            setinvmatrix <- function(inversematrix) invmatrix <- inversematrix
+              
+            getinvmatrix <- function() invmatrix
+          
+                       
+            list(set = set, get = get,
+                 setinvmatrix = setinvmatrix,
+                 getinvmatrix = getinvmatrix)
+    }
 
-## Write a short comment describing this function
+  cachematrix <- function(origmatrix, ...) {
+            invmatrix<- origmatrix$getinvmatrix()
+          
+            if(!is.null(invmatrix)) {
+                    message("getting cached data")
+                    return(invmatrix)
+            }
+            
+            
+            matr <- origmatrix$get()          
+            invmatrix <- solve(matr, ...)
+            origmatrix$setinvmatrix(invmatrix)
+            return(invmatrix)
+            
+            
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
-
+testinvmatrix <- function(mat){
+        ## mat: a matrix
+        
+        temp = makeMatrix(mat)
+        start.time = Sys.time()
+        cachematrix(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+        
+    
+        start.time = Sys.time()
+        cachematrix(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+        
+        
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+set.seed(12222)
+r = rnorm(1000000)
+mat1 = matrix(r, nrow=1000, ncol=1000)
+
+
+
